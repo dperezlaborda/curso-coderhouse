bda7c600cac47189e641dd9516cad0963f31af62
dperezlaborda <55368538+dperezlaborda@users.noreply.github.com>
2020-12-29 12:28:00 -0300

    se agrega contador en nuevo componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from './components/NavBar';
+import ItemCount from './components/ItemCount';
 
 function App() {
+
+  const [counter, setCounter] = useState(1);
+
+  const addProduct = () => {
+    setCounter(counter + 1);
+  }
+
+  const removeProduct = () => {
+    if (counter !== 1) {
+      setCounter(counter - 1)
+    } else {
+      console.log("no se puede")
+    }
+  }
+
   return (
     <div className="App">
       <NavBar />
+      <ItemCount
+        counter={counter}
+        addProduct={addProduct}
+        removeProduct={removeProduct}
+      />
     </div>
   );
 }
