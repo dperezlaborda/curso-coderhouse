4a89865724ab32e122a9c6897c003a84d7810bfa
dperezlaborda <55368538+dperezlaborda@users.noreply.github.com>
2021-01-03 16:50:58 -0300

    se cambia de estilo todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import NavBar from './components/NavBar';
+import NavBar2 from './components/NavBar2';
 import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <NavBar2 />
       <ItemListContainer greeting="Hola Dolores" name="Dolores" />
     </div>
   );
---
+++ src/components/ItemCount.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import '../styles/itemCount.css';
+import Container from 'react-bootstrap/Container';
+
+const ItemCount = ({ counter, addProduct, removeProduct, addCart, stock }) => {
+    return (
+        <Container>
+            <p className="mt-5">Silla Wassily</p>
+            <p>Stock disponible: {stock}</p>
+            <Row className="mx-3">
+                <Col xs={4} md={3} lg={2} className="px-4 d-flex justify-content-center counter">
+                    <Row className="bttn-border">
+                        <Button onClick={addProduct} className="noShadow">
+                            <FontAwesomeIcon icon={faPlus} />
+                        </Button>
+                        <p className="quantity">{counter}</p>
+                        <Button onClick={removeProduct} className="noShadow">
+                            <FontAwesomeIcon icon={faMinus} />
+                        </Button>
+                    </Row>
+                </Col>
+                <Col xs={4} md={9} lg={10} className="addCart">
+                    <Button onClick={addCart} className="text-uppercase addCart-btn noShadow">agregar al carrito</Button>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default ItemCount;
